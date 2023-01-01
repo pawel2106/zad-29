cc2046a7f9a749a23cd53c47cf77d0d75ea0f8a5
pawel2106 <115808901+pawel2106@users.noreply.github.com>
2023-01-01 13:25:26 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let firstNumber = 3
+let secondNumber = 14
+
+function dodaj(wart1, wart2) {
+
+  let wynik = wart1 + wart2;
+
+  return wynik;
+
+}
+
+let wartosc = dodaj(firstNumber, secondNumber);
+
+document.write("Wynikiem dodawania"+firstNumber+"+"+secondNumber+"  jest " + wartosc + ".");
